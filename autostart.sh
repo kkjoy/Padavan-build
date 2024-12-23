83a59745529216231a6b325e717e8d61e5a8ea84
lissom <kkjoy2617@gmail.com>
2024-12-23 22:14:35 +0800

    Update autostart.sh

--- autostart.sh
+++ autostart.sh
@@ -11,36 +11,3 @@ if [ $(nvram get sdns_enable) = 1 ] ; then
 logger -t "自动启动" "正在启动SmartDns"
 /usr/bin/smartdns.sh start
 fi
-
-logger -t "自动启动" "正在检查路由是否已连接互联网！"
-count=0
-while :
-do
-	ping -c 1 -W 1 -q www.baidu.com 1>/dev/null 2>&1
-	if [ "$?" == "0" ]; then
-		break
-	fi
-	ping -c 1 -W 1 -q 202.108.22.5 1>/dev/null 2>&1
-	if [ "$?" == "0" ]; then
-		break
-	fi
-	sleep 5
-	ping -c 1 -W 1 -q www.google.com 1>/dev/null 2>&1
-	if [ "$?" == "0" ]; then
-		break
-	fi
-	ping -c 1 -W 1 -q 8.8.8.8 1>/dev/null 2>&1
-	if [ "$?" == "0" ]; then
-		break
-	fi
-	sleep 5
-	count=$((count+1))
-	if [ $count -gt 18 ]; then
-		break
-	fi
-done
-
-logger -t "自动启动" "正在启动tailscale"
-cd /tmp
-tailscaled -state /etc/storage/tailscaled.state &
-logger -t "自动启动" "启动tailscale完成"
