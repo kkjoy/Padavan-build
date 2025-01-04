038b903c57790b66e4bb6ee790c91e5ddbd60fed
lissom <kkjoy2617@gmail.com>
2025-01-04 22:47:03 +0800

    Update copyscripts.sh

--- copyscripts.sh
+++ copyscripts.sh
@@ -7,8 +7,8 @@ if [ ! -f "/etc/storage/smartdns_custom.conf" ] ; then
 cp -rf /etc_ro/smartdns_custom.conf /etc/storage/
 chmod 755 "/etc/storage/smartdns_custom.conf"
 fi
-if [ ! -f "/etc/storage/gfwlist.txt" ] ; then
-mkdir -p /etc/storage/gfwlist
-cp -rf /etc_ro/gfwlist.txt /etc/storage/gfwlist.txt
-chmod 755 "/etc/storage/gfwlist/gfwlist_list.conf"
+if [ ! -f "/etc/storage/dnsmasq/gfwlist.txt" ] ; then
+mkdir -p /etc/storage/dnsmasq/
+cp -rf /etc_ro/gfwlist.txt /etc/storage/dnsmasq/gfwlist.txt
+chmod 755 "/etc/storage/dnsmasq/gfwlist.txt"
 fi
