8adfb6d154aa2b6fe3007ebd85f10622b4cf9ada
lissom <kkjoy2617@gmail.com>
2026-08-07 15:41:31 +0800

    Update smartdns.sh

--- smartdns/smartdns.sh
+++ smartdns/smartdns.sh
@@ -1,15 +1,23 @@
 #!/bin/sh
 
-APP=/usr/bin/smartdns
-CONF=/etc/storage/smartdns_custom.conf
+if [ -x /etc/storage/smartdns ]; then
+    APP=/etc/storage/smartdns
+elif [ -x /usr/bin/smartdns ]; then
+    APP=/usr/bin/smartdns
+else
+    exit 1
+fi
 
-NUM=`ps | grep -w $APP | grep -v grep | wc -l`
-if [ "$NUM" -lt "1" ];then
-  $APP -c $CONF
-elif [ "$NUM" -gt "1" ];then
-  pgrep -f $APP | xargs kill -9
-  sleep 1s
-  $APP -c $CONF
+CONF=/etc/storage/smartdns_custom.conf
+APP_NAME=$(basename "$APP")
+NUM=$(pgrep -x "$APP_NAME" 2>/dev/null | wc -l)
+
+if [ "$NUM" -lt "1" ]; then
+    $APP -c "$CONF"
+elif [ "$NUM" -gt "1" ]; then
+    pgrep -x "$APP_NAME" | xargs kill -9 2>/dev/null
+    sleep 1
+    $APP -c "$CONF"
 fi
 
 exit 0
